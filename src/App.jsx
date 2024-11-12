b2db7352d85e92262b884e3862db180c0fc72777
Abeen Khanal <abeenkhanal16@gmail.com>
2024-11-12 11:21:21 +0545

    somthing modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Nav from './Component/Navbar/nav'
-import Sidebar from './Component/Sidebar/side'
+import Nav from './Component/Common/Navbar/nav'
+import Sidebar from './Component/Common/Sidebar/side'
 // import Side from './Component/Sidebar/side'
 
 function App() {
